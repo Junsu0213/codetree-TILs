e468e546e1b7b0988aa60afdd5fcb329a56d6975
Junsu Park <128777619+Junsu0213@users.noreply.github.com>
2024-06-07 18:01:11 +0900

    Title: 마법의 숲 탐색, Time: 385ms, Memory: 29MB, Status: Passed - CodeTree

--- 240607/마법의 숲 탐색/magical-forest-exploration.py
+++ 240607/마법의 숲 탐색/magical-forest-exploration.py
@@ -1,99 +1,93 @@
 import sys
-input = sys.stdin.readline
 from collections import deque
 
+input = sys.stdin.readline
 
-def bfs(visited, ri, ci):
-    visited_ = [[False] * c for _ in range(r + 3)]
-    x = [1, -1, 0, 0]
-    y = [0, 0, -1, 1]
+
+def bfs(maps, ri, ci):
+    visited = [[False]*c for _ in range(r+2)]
     q = deque()
     q.append((ri, ci))
-    rmax = ri - 1
+    dy = [1, -1, 0, 0]
+    dx = [0, 0, 1, -1]
+    rmax = ri
+
     while q:
         ri, ci = q.popleft()
-        idx = visited[ri][ci]
+        visited[ri][ci] = True
+        idx = maps[ri][ci]
         if idx == 3:
-            rmax = max(rmax, ri - 1)
-        visited_[ri][ci] = True
+            rmax = max(rmax, ri)
 
-        for k in range(4):
-            dx = ri + x[k]
-            dy = ci + y[k]
-            if 0 <= dx <= r+1 and 0 <= dy <= c-1 and (idx == 2 or idx == 3) and visited[dx][dy] != 0 and visited_[dx][dy] == False:
-                visited_[dx][dy] = True
-                q.append((dx, dy))
-            elif 0 <= dx <= r+1 and 0 <= dy <= c-1 and idx == 1 and visited[dx][dy] == 3 and visited[dx][dy] != 0 and visited_[dx][dy] == False:
-                visited_[dx][dy] = True
-                q.append((dx, dy))
-    return rmax
+        for i in range(4):
+            y = ri + dy[i]
+            x = ci + dx[i]
+            if 2 <= y < r+2 and 0 <= x < c and (idx == 3 or idx == 2) and maps[y][x] != 0 and visited[y][x] == False:
+                q.append((y, x))
+                visited[y][x] = True
+            elif 2 <= y < r+2 and 0 <= x < c and idx == 1 and maps[y][x] == 3 and visited[y][x] == False:
+                q.append((y, x))
+                visited[y][x] = True
+    return rmax 
+     
 
-
-def g_down(visited, r, c, ci, di):
+def g_move(maps, r, c, ci, di):
     ri = 0
+
     while True:
-        gn, ge, gs, gw = visited[ri - 1][ci], visited[ri][ci + 1], visited[ri + 1][ci], visited[ri][ci - 1]
-
+        # stop
+        if ri == r:
+            break
+        ge, gs, gw = maps[ri + 1][ci + 1], maps[ri + 2][ci], maps[ri + 1][ci - 1]        
         # down
-        if ge == 0 and gs == 0 and gw == 0:
+        if ri < r and ge == 0 and gs == 0 and gw == 0:
             ri += 1
-            if ri == r + 1 and visited[ri][ci + 1] == 0 and visited[ri + 1][ci] == 0 and visited[ri][ci - 1] == 0:
-                break
-
         # left
-        elif gs != 0 and gw == 0 and ci >= 2 and visited[ri + 1][ci - 1] == 0 and visited[ri][ci - 2] == 0:
+        elif ci > 1 and (gs != 0 or ge != 0) and gw == 0 and maps[ri][ci - 2] == 0 and maps[ri + 1][ci - 2] == 0 and maps[ri + 2][ci - 1] == 0:
             ci -= 1
             di -= 1
             if di < 0:
                 di = 3
-
         # right
-        elif gs != 0 and ge == 0 and ci <= c - 3 and visited[ri + 1][ci + 1] == 0 and visited[ri][
-            ci + 2] == 0:
+        elif ci < c - 2 and (gs != 0 or gw != 0) and ge == 0 and maps[ri][ci + 2] == 0 and maps[ri + 1][ci + 2] == 0 and maps[ri + 2][ci + 1] == 0:
             ci += 1
             di += 1
             if di > 3:
                 di = 0
-
         else:
-            ri -= 1
             break
 
-    # map reset
     if ri < 3:
-        visited = [[0] * c for _ in range(r + 3)]
+        maps = [[0]*c for _ in range(r+2)]
         rmax = 0
-
     else:
-        visited[ri][ci] = 3
-        visited[ri + 1][ci] = 1
-        visited[ri - 1][ci] = 1
-        visited[ri][ci + 1] = 1
-        visited[ri][ci - 1] = 1
-
+        maps[ri][ci] = 3
+        maps[ri - 1][ci] = 1
+        maps[ri][ci + 1] = 1
+        maps[ri + 1][ci] = 1
+        maps[ri][ci - 1] = 1
+        
+        # exit
         if di == 0:
-            visited[ri - 1][ci] = 2
+            maps[ri - 1][ci] = 2
         elif di == 1:
-            visited[ri][ci + 1] = 2
+            maps[ri][ci + 1] = 2
         elif di == 2:
-            visited[ri + 1][ci] = 2
+            maps[ri + 1][ci] = 2
         elif di == 3:
-            visited[ri][ci - 1] = 2
-
-        rmax = bfs(visited, ri, ci)
-
-    return visited, rmax
+            maps[ri][ci - 1] = 2
+        
+        rmax = bfs(maps, ri, ci)
+    return maps, rmax
 
 
 r, c, k = map(int, input().split())
-visited = [[0] * c for _ in range(r + 3)]
-
+maps = [[0]*c for _ in range(r+2)]
 answer = 0
+
 for _ in range(k):
     ci, di = map(int, input().split())
-    ci -= 1
-    visited, rmax = g_down(visited, r, c, ci, di)
-
+    maps, rmax = g_move(maps, r, c, ci - 1, di)
     answer += rmax
 
 print(answer)
